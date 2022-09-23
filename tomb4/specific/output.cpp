bee287e7291366ec0c0de53bb1a810c5fe105eb6
asasas9500 <asasas9500@gmail.com>
2022-09-22 23:21:34 -0300

    Fix compatibility with patches in phd_PutPolygons()

--- tomb4/specific/output.cpp
+++ tomb4/specific/output.cpp
@@ -77,255 +77,192 @@ SPRITESTRUCT* CustEnvMap(short* objptr)
 	return envmap;
 }
 
-void phd_PutPolygons(short* objptr, long clip)
+const float f1 = 2.0F;
+const float f2 = 256.0F;
+
+static float w, h;
+
+__declspec(naked) void phd_PutPolygons1()
 {
-	MESH_DATA* mesh;
-	SPRITESTRUCT* envmap_sprite;
-	D3DTLVERTEX* v;
-	TEXTURESTRUCT* pTex;
-	D3DVECTOR normals[4];
-	TEXTURESTRUCT envmap_texture;
-	short* quad;
-	short* tri;
-	long clrbak[4];
-	long spcbak[4];
-	long num;
-	ushort drawbak;
-	bool envmap;
-	float w, h; // new variables
-
-	SetD3DViewMatrix();
-	mesh = (MESH_DATA*)objptr;
-
-
-	if (objptr)
+	__asm
 	{
-		// We don't need this. It gets overwritten anyway.
-		/*
-		if (objptr == meshes[objects[LARA_DOUBLE].mesh_index] || objptr == meshes[objects[LARA_DOUBLE].mesh_index + 2])
-			envmap_sprite = &spriteinfo[objects[SKY_GRAPHICS].mesh_index];
-		else
-			envmap_sprite = &spriteinfo[objects[DEFAULT_SPRITES].mesh_index + 11];
-		*/
+		push ebx
+		push esi
+		push ebp
+		call CustEnvMap
+		add esp, 4h
+		mov ebx, eax
+		movzx eax, WORD PTR [ebx + 4h]
+		shr eax, 8h
+		mov DWORD PTR [w], eax
+		fild DWORD PTR [w]
+		fdiv DWORD PTR [f1]
+		fdiv DWORD PTR [f2]
+		fstp DWORD PTR [w]
+		movzx eax, WORD PTR [ebx + 6h]
+		shr eax, 8h
+		mov DWORD PTR [h], eax
+		fild DWORD PTR [h]
+		fdiv DWORD PTR [f1]
+		fdiv DWORD PTR [f2]
+		fstp DWORD PTR [h]
+		mov edx, 47BB8Ch
+		jmp edx
+	}
+}
 
-		envmap_sprite = CustEnvMap(objptr);
+__declspec(naked) void phd_PutPolygons2()
+{
+	__asm
+	{
+		add esp, 20h
+		fld DWORD PTR [esp + 5Ch]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 5Ch]
+		fld DWORD PTR [esp + 60h]
+		fmul DWORD PTR [h]
+		fstp DWORD PTR [esp + 60h]
+		fld DWORD PTR [esp + 64h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 64h]
+		fld DWORD PTR [esp + 1Ch]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 1Ch]
+		fld DWORD PTR [esp + 20h]
+		fmul DWORD PTR [h]
+		fstp DWORD PTR [esp + 20h]
+		fld DWORD PTR [esp + 24h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 24h]
+		fld DWORD PTR [esp + 34h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 34h]
+		fld DWORD PTR [esp + 38h]
+		fmul DWORD PTR [h]
+		fstp DWORD PTR [esp + 38h]
+		fld DWORD PTR [esp + 3Ch]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 3Ch]
+		fld DWORD PTR [esp + 28h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 28h]
+		fld DWORD PTR [esp + 2Ch]
+		fmul DWORD PTR [h]
+		fstp DWORD PTR [esp + 2Ch]
+		fld DWORD PTR [esp + 30h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 30h]
+		mov WORD PTR [esp + 78h], 2h
+		mov ax, WORD PTR [ebx]
+		mov WORD PTR [esp + 7Ah], ax
+		mov ax, WORD PTR [edi + 4h]
+		mov WORD PTR [esp + 7Ch], ax
+		fld DWORD PTR [esp + 5Ch]
+		fadd DWORD PTR [ebx + 8h]
+		fadd DWORD PTR [w]
+		fstp DWORD PTR [esp + 7Eh]
+		fld DWORD PTR [esp + 60h]
+		fadd DWORD PTR [ebx + 0Ch]
+		fadd DWORD PTR [h]
+		fstp DWORD PTR [esp + 82h]
+		fld DWORD PTR [esp + 1Ch]
+		fadd DWORD PTR [ebx + 8h]
+		fadd DWORD PTR [w]
+		fstp DWORD PTR [esp + 86h]
+		fld DWORD PTR [esp + 20h]
+		fadd DWORD PTR [ebx + 0Ch]
+		fadd DWORD PTR [h]
+		fstp DWORD PTR [esp + 8Ah]
+		fld DWORD PTR [esp + 34h]
+		fadd DWORD PTR [ebx + 8h]
+		fadd DWORD PTR [w]
+		fstp DWORD PTR [esp + 8Eh]
+		fld DWORD PTR [esp + 38h]
+		fadd DWORD PTR [ebx + 0Ch]
+		fadd DWORD PTR [h]
+		fstp DWORD PTR [esp + 92h]
+		fld DWORD PTR [esp + 28h]
+		fadd DWORD PTR [ebx + 8h]
+		fadd DWORD PTR [w]
+		fstp DWORD PTR [esp + 96h]
+		fld DWORD PTR [esp + 2Ch]
+		fadd DWORD PTR [ebx + 0Ch]
+		fadd DWORD PTR [h]
+		fstp DWORD PTR [esp + 9Ah]
+		mov edx, 47BF60h
+		jmp edx
+	}
+}
 
-		// Calculate sprite size to allow for arbitrary sprite sizes. No longer limited to 64x64
-		w = float(envmap_sprite->width >> 8);
-		w /= 2.0F;                                //half the size
-		w /= 256.0F;                            //by the tpage size
-		h = float(envmap_sprite->height >> 8);
-		h /= 2.0F;
-		h /= 256.0F;
-
-		if (GlobalAmbient)
-		{
-			ClearObjectLighting();
-			ClearDynamicLighting();
-			App.dx.lpD3DDevice->SetLightState(D3DLIGHTSTATE_AMBIENT, GlobalAmbient);
-			GlobalAmbient = 0;
-		}
-		else if (mesh->prelight)
-		{
-			ClearObjectLighting();
-			InitItemDynamicLighting(current_item);
-		}
-		else
-			InitObjectLighting(current_item);
-
-		if (mesh->nVerts)
-			DXAttempt(DestVB->ProcessVertices(D3DVOP_LIGHT | D3DVOP_TRANSFORM, 0, mesh->nVerts, mesh->SourceVB, 0, App.dx._lpD3DDevice, 0));
-
-		DestVB->Lock(DDLOCK_READONLY, (void**)&v, 0);
-		clip_left = f_left;
-		clip_top = f_top;
-		clip_right = f_right;
-		clip_bottom = f_bottom;
-
-		if (mesh->nVerts)
-		{
-			bWaterEffect = camera.underwater;
-			ProjectVerts(mesh->nVerts, v, clipflags);
-
-			if (mesh->prelight)
-				PrelightVerts(mesh->nVerts, v, mesh);
-			else
-				CalcVertsColorSplitMMX(mesh->nVerts, v);
-		}
-
-		quad = mesh->gt4;
-
-		for (int i = 0; i < mesh->ngt4; i++, quad += 6)
-		{
-			pTex = &textinfo[quad[4] & 0x7FFF];
-			envmap = 0;
-			drawbak = pTex->drawtype;
-
-			if (quad[5] & 1)
-				pTex->drawtype = 2;
-
-			if (quad[5] & 2)
-			{
-				envmap = 1;
-				num = (quad[5] >> 2) & 0x1F;
-				normals[0] = mesh->Normals[quad[0]];
-				normals[1] = mesh->Normals[quad[1]];
-				normals[2] = mesh->Normals[quad[2]];
-				normals[3] = mesh->Normals[quad[3]];
-				D3DTransform(&normals[0], &D3DMView);
-				D3DTransform(&normals[1], &D3DMView);
-				D3DTransform(&normals[2], &D3DMView);
-				D3DTransform(&normals[3], &D3DMView);
-
-				for (int i = 0; i < 4; i++)
-				{
-					normals[i].x *= w;
-					normals[i].y *= h;
-					normals[i].z *= w;
-				}
-
-				envmap_texture.drawtype = 2;
-				envmap_texture.flag = pTex->flag;
-				envmap_texture.tpage = envmap_sprite->tpage;
-				envmap_texture.u1 = normals[0].x + envmap_sprite->x1 + w;
-				envmap_texture.v1 = normals[0].y + envmap_sprite->y1 + h;
-				envmap_texture.u2 = normals[1].x + envmap_sprite->x1 + w;
-				envmap_texture.v2 = normals[1].y + envmap_sprite->y1 + h;
-				envmap_texture.u3 = normals[2].x + envmap_sprite->x1 + w;
-				envmap_texture.v3 = normals[2].y + envmap_sprite->y1 + h;
-				envmap_texture.u4 = normals[3].x + envmap_sprite->x1 + w;
-				envmap_texture.v4 = normals[3].y + envmap_sprite->y1 + h;
-			}
-
-			if (GlobalAlpha == 0xFF000000)
-			{
-				if (!pTex->drawtype)
-					AddQuadZBuffer(v, quad[0], quad[1], quad[2], quad[3], pTex, 0);
-				else if (pTex->drawtype <= 2)
-					AddQuadSorted(v, quad[0], quad[1], quad[2], quad[3], pTex, 0);
-
-				if (envmap)
-				{
-					clrbak[0] = v[quad[0]].color;
-					clrbak[1] = v[quad[1]].color;
-					clrbak[2] = v[quad[2]].color;
-					clrbak[3] = v[quad[3]].color;
-					spcbak[0] = v[quad[0]].specular;
-					spcbak[1] = v[quad[1]].specular;
-					spcbak[2] = v[quad[2]].specular;
-					spcbak[3] = v[quad[3]].specular;
-					RGB_M(v[quad[0]].color, num);
-					RGB_M(v[quad[1]].color, num);
-					RGB_M(v[quad[2]].color, num);
-					RGB_M(v[quad[3]].color, num);
-					RGB_M(v[quad[0]].specular, num);
-					RGB_M(v[quad[1]].specular, num);
-					RGB_M(v[quad[2]].specular, num);
-					RGB_M(v[quad[3]].specular, num);
-					AddQuadSorted(v, quad[0], quad[1], quad[2], quad[3], &envmap_texture, 0);
-					v[quad[0]].color = clrbak[0];
-					v[quad[1]].color = clrbak[1];
-					v[quad[2]].color = clrbak[2];
-					v[quad[3]].color = clrbak[3];
-					v[quad[0]].specular = spcbak[0];
-					v[quad[1]].specular = spcbak[1];
-					v[quad[2]].specular = spcbak[2];
-					v[quad[3]].specular = spcbak[3];
-				}
-			}
-			else
-			{
-				pTex->drawtype = 7;
-				AddQuadSorted(v, quad[0], quad[1], quad[2], quad[3], pTex, 0);
-			}
-
-			pTex->drawtype = drawbak;
-		}
-
-		tri = mesh->gt3;
-
-		for (int i = 0; i < mesh->ngt3; i++, tri += 5)
-		{
-			pTex = &textinfo[tri[3] & 0x7FFF];
-			envmap = 0;
-			drawbak = pTex->drawtype;
-
-			if (tri[4] & 1)
-				pTex->drawtype = 2;
-
-			if (tri[4] & 2)
-			{
-				envmap = 1;
-				num = (tri[4] >> 2) & 0x1F;
-				normals[0] = mesh->Normals[tri[0]];
-				normals[1] = mesh->Normals[tri[1]];
-				normals[2] = mesh->Normals[tri[2]];
-				D3DTransform(&normals[0], &D3DMView);
-				D3DTransform(&normals[1], &D3DMView);
-				D3DTransform(&normals[2], &D3DMView);
-
-				for (int i = 0; i < 3; i++)
-				{
-					normals[i].x *= w;
-					normals[i].y *= h;
-					normals[i].z *= w;
-				}
-
-				envmap_texture.drawtype = 2;
-				envmap_texture.flag = pTex->flag;
-				envmap_texture.tpage = envmap_sprite->tpage;
-				envmap_texture.u1 = normals[0].x + envmap_sprite->x1 + w;
-				envmap_texture.v1 = normals[0].y + envmap_sprite->y1 + h;
-				envmap_texture.u2 = normals[1].x + envmap_sprite->x1 + w;
-				envmap_texture.v2 = normals[1].y + envmap_sprite->y1 + h;
-				envmap_texture.u3 = normals[2].x + envmap_sprite->x1 + w;
-				envmap_texture.v3 = normals[2].y + envmap_sprite->y1 + h;
-			}
-
-			if (GlobalAlpha == 0xFF000000)
-			{
-				if (!pTex->drawtype)
-					AddTriZBuffer(v, tri[0], tri[1], tri[2], pTex, 0);
-				else if (pTex->drawtype <= 2)
-					AddTriSorted(v, tri[0], tri[1], tri[2], pTex, 0);
-
-				if (envmap)
-				{
-					clrbak[0] = v[tri[0]].color;
-					clrbak[1] = v[tri[1]].color;
-					clrbak[2] = v[tri[2]].color;
-					spcbak[0] = v[tri[0]].specular;
-					spcbak[1] = v[tri[1]].specular;
-					spcbak[2] = v[tri[2]].specular;
-					RGB_M(v[tri[0]].color, num);
-					RGB_M(v[tri[1]].color, num);
-					RGB_M(v[tri[2]].color, num);
-					RGB_M(v[tri[0]].specular, num);
-					RGB_M(v[tri[1]].specular, num);
-					RGB_M(v[tri[2]].specular, num);
-					AddTriSorted(v, tri[0], tri[1], tri[2], &envmap_texture, 0);
-					v[tri[0]].color = clrbak[0];
-					v[tri[1]].color = clrbak[1];
-					v[tri[2]].color = clrbak[2];
-					v[tri[0]].specular = spcbak[0];
-					v[tri[1]].specular = spcbak[1];
-					v[tri[2]].specular = spcbak[2];
-				}
-			}
-			else
-			{
-				pTex->drawtype = 7;
-				AddTriSorted(v, tri[0], tri[1], tri[2], pTex, 0);
-			}
-
-			pTex->drawtype = drawbak;
-		}
-
-		DestVB->Unlock();
+__declspec(naked) void phd_PutPolygons3()
+{
+	__asm
+	{
+		add esp, 18h
+		fld DWORD PTR [esp + 28h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 28h]
+		fld DWORD PTR [esp + 2Ch]
+		fmul DWORD PTR [h]
+		fstp DWORD PTR [esp + 2Ch]
+		fld DWORD PTR [esp + 30h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 30h]
+		fld DWORD PTR [esp + 34h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 34h]
+		fld DWORD PTR [esp + 38h]
+		fmul DWORD PTR [h]
+		fstp DWORD PTR [esp + 38h]
+		fld DWORD PTR [esp + 3Ch]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 3Ch]
+		fld DWORD PTR [esp + 1Ch]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 1Ch]
+		fld DWORD PTR [esp + 20h]
+		fmul DWORD PTR [h]
+		fstp DWORD PTR [esp + 20h]
+		fld DWORD PTR [esp + 24h]
+		fmul DWORD PTR [w]
+		fstp DWORD PTR [esp + 24h]
+		mov WORD PTR [esp + 78h], 2h
+		mov ax, WORD PTR [ebx]
+		mov WORD PTR [esp + 7Ah], ax
+		mov ax, WORD PTR [edi + 4h]
+		mov WORD PTR [esp + 7Ch], ax
+		fld DWORD PTR [esp + 28h]
+		fadd DWORD PTR [ebx + 8h]
+		fadd DWORD PTR [w]
+		fstp DWORD PTR [esp + 7Eh]
+		fld DWORD PTR [esp + 2Ch]
+		fadd DWORD PTR [ebx + 0Ch]
+		fadd DWORD PTR [h]
+		fstp DWORD PTR [esp + 82h]
+		fld DWORD PTR [esp + 34h]
+		fadd DWORD PTR [ebx + 8h]
+		fadd DWORD PTR [w]
+		fstp DWORD PTR [esp + 86h]
+		fld DWORD PTR [esp + 38h]
+		fadd DWORD PTR [ebx + 0Ch]
+		fadd DWORD PTR [h]
+		fstp DWORD PTR [esp + 8Ah]
+		fld DWORD PTR [esp + 1Ch]
+		fadd DWORD PTR [ebx + 8h]
+		fadd DWORD PTR [w]
+		fstp DWORD PTR [esp + 8Eh]
+		fld DWORD PTR [esp + 20h]
+		fadd DWORD PTR [ebx + 0Ch]
+		fadd DWORD PTR [h]
+		fstp DWORD PTR [esp + 92h]
+		mov edx, 47C5D0h
+		jmp edx
 	}
 }
 
 void inject_output(bool replace)
 {
-	INJECT(0x0047bb30, phd_PutPolygons, replace);
+	INJECT(0x0047BB4E, phd_PutPolygons1, replace);
+	INJECT(0x0047BDCE, phd_PutPolygons2, replace);
+	INJECT(0x0047C49B, phd_PutPolygons3, replace);
 }
